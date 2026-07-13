70dd1d9efba7dc6c5ef1a36b07ae0428969824c9
nino-rgb <pnekoneko907@gmail.com>
2026-07-14 01:28:47 +0900

    04 抽象化

---
+++ src/4_abstraction/sampleCode.ts
@@ -0,0 +1,36 @@
+//インターフェースの作成
+interface Payment {
+  processPayment(amount: number): void;
+}
+
+//クレカ決済クラス
+class CreditCardPayment implements Payment {
+  processPayment(amount: number): void {
+    console.log(`クレジットカードで${amount}円の支払い処理をしています...`);
+  }
+}
+
+class PayPalPayment implements Payment {
+  processPayment(amount: number): void {
+    console.log(`PayPalで${amount}円の支払い処理をしています...`);
+  }
+}
+
+class PayPayPayment implements Payment {
+  processPayment(amount: number): void {
+    console.log(`PayPayで${amount}円 払います`);
+  }
+}
+
+const totalAmount = 5000;
+
+const myPayments: Payment[] = [new CreditCardPayment(), new PayPalPayment(), new PayPayPayment()];
+
+console.log("--- 決済テスト開始 ---");
+for (const payment of myPayments) {
+  payment.processPayment(totalAmount);
+}
+//実行結果
+//--- 決済テスト開始 ---
+// クレジットカードで 5000円 の支払いを処理しています...
+// PayPalで 5000円 の支払いを処理しています...
